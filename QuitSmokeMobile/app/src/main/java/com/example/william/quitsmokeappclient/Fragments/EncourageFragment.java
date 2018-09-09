6690b2ecb7a30b2689e6044e2da2aa7202662728
Unknown <swan0005@student.monash.edu>
2018-09-10 02:26:40 +1000

    initial panic button function

--- QuitSmokeMobile/app/src/main/java/com/example/william/quitsmokeappclient/Fragments/EncourageFragment.java
+++ QuitSmokeMobile/app/src/main/java/com/example/william/quitsmokeappclient/Fragments/EncourageFragment.java
@@ -1,46 +1,52 @@
 package com.example.william.quitsmokeappclient.Fragments;
 
-import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
 import com.dinuscxj.progressbar.CircleProgressBar;
 import com.example.william.quitsmokeappclient.R;
+
 import clientservice.QuitSmokeClientUtils;
 import clientservice.factory.GetCurrentPlanFactorial;
 
-public class SmokerMainFragment extends Fragment {
-    private CircleProgressBar mCustomProgressBar;
-    private Button btnPanicButton;
+public class EncourageFragment extends Fragment {
+    private Button btnYes;
+    private Button btnNo;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.smoker_main_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_encouragement, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // set progress bar
-        mCustomProgressBar = (CircleProgressBar)view.findViewById(R.id.custom_progress);
-        // get current proceeding plan
-        GetCurrentPlanFactorial getCurrentPlanFactorial = new GetCurrentPlanFactorial(getActivity(), QuitSmokeClientUtils.getUid(), mCustomProgressBar);
-        getCurrentPlanFactorial.execute();
+        // get buttons
+        btnYes = view.findViewById(R.id.yesforencouragement);
+        btnNo = view.findViewById(R.id.noforencouragement);
 
-        // get panic button
-        btnPanicButton = (Button)view.findViewById(R.id.buttonforquitter);
-        btnPanicButton.setOnClickListener(new View.OnClickListener() {
+        // add actions
+        btnNo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 FragmentManager fragmentManager = getFragmentManager();
-                fragmentManager.beginTransaction().replace(R.id.content_frame, new EncourageFragment()).commit();
+                fragmentManager.beginTransaction().replace(R.id.content_frame, new SmokerMainFragment()).commit();
+            }
+        });
+
+        btnYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
