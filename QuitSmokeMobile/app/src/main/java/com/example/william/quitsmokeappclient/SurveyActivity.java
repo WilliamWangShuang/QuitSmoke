dd6c2b2ad1c112a235041e27c6236dc7a1a01572
Unknown <swan0005@student.monash.edu>
2018-09-30 13:49:21 +1000

    check if network connected when launch app

--- QuitSmokeMobile/app/src/main/java/com/example/william/quitsmokeappclient/SurveyActivity.java
+++ QuitSmokeMobile/app/src/main/java/com/example/william/quitsmokeappclient/SurveyActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -13,6 +15,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.example.william.quitsmokeappclient.Fragments.SurveyErrorFragment;
 import com.example.william.quitsmokeappclient.Interface.IApprovePlanAsyncResponse;
 
@@ -29,6 +33,7 @@ public class SurveyActivity extends AppCompatActivity implements IApprovePlanAsy
     private Spinner ddlAge;
     private Spinner ddlGender;
     private Button btnNext;
+    private TextView tvLaunch;
     private boolean isSmokePerDayValid;
     private boolean isGenderValid;
     private boolean isAgeValid;
@@ -47,6 +52,7 @@ public class SurveyActivity extends AppCompatActivity implements IApprovePlanAsy
         ddlAge = (Spinner)findViewById((R.id.survey_age));
         ddlGender = (Spinner)findViewById((R.id.survey_gender));
         btnNext = (Button)findViewById(R.id.btn_servey_next);
+        tvLaunch = findViewById(R.id.main_page_login);
 
         // set age spinner
         // Get reference of widgets from XML layout
@@ -76,69 +82,78 @@ public class SurveyActivity extends AppCompatActivity implements IApprovePlanAsy
         spinnerGenderAdapter.setDropDownViewResource(R.layout.spinner_item);
         ddlGender.setAdapter(spinnerGenderAdapter);
 
-        TextView tvLaunch = findViewById(R.id.main_page_login);
-        tvLaunch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // get email and pwd from shared preference. If exist, direct sign in. Otherwise, go to login page
-                SharedPreferences sharedPreferences = getSharedPreferences("MyPref", Context.MODE_PRIVATE);
-                String emailInPreference = sharedPreferences.getString("email", "");
-                String pwdInPreference = sharedPreferences.getString("pwd", "");
-                if (emailInPreference == null
-                        || "".equals(emailInPreference)
-                        || pwdInPreference == null
-                        || "".equals(pwdInPreference)) {
-                    // go to register activity
-                    Intent intent = new Intent(SurveyActivity.this, LaunchActivity.class);
-                    startActivityForResult(intent, 1);
-                } else {
-                    LoginFactorial loginFactorial = new LoginFactorial(currActivity, emailInPreference, pwdInPreference, true);
-                    loginFactorial.delegate = SurveyActivity.this;
-                    loginFactorial.execute();
-                }
-            }
-        });
+        // check if network connected, if yes, enable buttons
+        if (haveNetworkConnection()) {
+            tvLaunch.setEnabled(true);
+            btnNext.setEnabled(true);
 
-        // sync DB with SQLite only once each time app launch. After launch, it should not do this sync again when go back to this activity
-        boolean isSmokeFreeZoneSync = QuitSmokeClientUtils.isIsSmokeFreeZoneSync();
-        // start load no smoke place receiver
-        if (!isSmokeFreeZoneSync) {
-            Log.d("QuitSmokeDebug", "Start Sychronize Smoke Free Zone data between SQLite and Firebase.");
-            syncNoSmokePlaceReceiver = new SyncNoSmokePlaceReceiver(this);
-            QuitSmokeClientUtils.setIsSmokeFreeZoneSync(true);
+            tvLaunch.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // get email and pwd from shared preference. If exist, direct sign in. Otherwise, go to login page
+                    SharedPreferences sharedPreferences = getSharedPreferences("MyPref", Context.MODE_PRIVATE);
+                    String emailInPreference = sharedPreferences.getString("email", "");
+                    String pwdInPreference = sharedPreferences.getString("pwd", "");
+                    if (emailInPreference == null
+                            || "".equals(emailInPreference)
+                            || pwdInPreference == null
+                            || "".equals(pwdInPreference)) {
+                        // go to register activity
+                        Intent intent = new Intent(SurveyActivity.this, LaunchActivity.class);
+                        startActivityForResult(intent, 1);
+                    } else {
+                        LoginFactorial loginFactorial = new LoginFactorial(currActivity, emailInPreference, pwdInPreference, true);
+                        loginFactorial.delegate = SurveyActivity.this;
+                        loginFactorial.execute();
+                    }
+                }
+            });
+
+            // sync DB with SQLite only once each time app launch. After launch, it should not do this sync again when go back to this activity
+            boolean isSmokeFreeZoneSync = QuitSmokeClientUtils.isIsSmokeFreeZoneSync();
+            // start load no smoke place receiver
+            if (!isSmokeFreeZoneSync) {
+                Log.d("QuitSmokeDebug", "Start Sychronize Smoke Free Zone data between SQLite and Firebase.");
+                syncNoSmokePlaceReceiver = new SyncNoSmokePlaceReceiver(this);
+                QuitSmokeClientUtils.setIsSmokeFreeZoneSync(true);
+            }
+
+            // set onclick on button 'next'
+            btnNext.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // get user input value
+                    String str_smoke_per_day = txtSmokePerDay.getText().toString();
+                    String gender = ddlGender.getSelectedItem().toString();
+                    String str_age = ddlAge.getSelectedItem().toString();
+                    Intent intent = new Intent(SurveyActivity.this, SurveyResultActivity.class);
+                    // if UI validate true, go to result activity, else show error message dialog
+                    if (validateUI(str_smoke_per_day, gender, str_age)) {
+                        int smoke_per_day = Integer.parseInt(str_smoke_per_day);
+                        int age = Integer.parseInt(str_age);
+                        intent.putExtra(v.getResources().getString(R.string.survey_smoke), smoke_per_day);
+                        intent.putExtra(v.getResources().getString(R.string.survery_age), age);
+                        intent.putExtra(v.getResources().getString(R.string.survey_gender), gender);
+
+                        // start next seal page
+                        startActivityForResult(intent, 1);
+                    } else {
+                        SurveyErrorFragment newFragment = new SurveyErrorFragment();
+                        Bundle bundle = new Bundle();
+                        // pass validation result to dialog
+                        bundle.putBoolean("isAgeValid", isAgeValid);
+                        bundle.putBoolean("isSmokeValid", isSmokePerDayValid);
+                        bundle.putBoolean("isGenderValid", isGenderValid);
+                        newFragment.setArguments(bundle);
+                        newFragment.show(getSupportFragmentManager(), "missiles");
+                    }
+                }
+            });
+        } else {
+            tvLaunch.setEnabled(false);
+            btnNext.setEnabled(false);
+            Toast.makeText(this, getResources().getString(R.string.no_network_msg), Toast.LENGTH_LONG).show();
         }
-
-        // set onclick on button 'next'
-        btnNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // get user input value
-                String str_smoke_per_day = txtSmokePerDay.getText().toString();
-                String gender = ddlGender.getSelectedItem().toString();
-                String str_age = ddlAge.getSelectedItem().toString();
-                Intent intent = new Intent(SurveyActivity.this, SurveyResultActivity.class);
-                // if UI validate true, go to result activity, else show error message dialog
-                if (validateUI(str_smoke_per_day, gender, str_age)) {
-                    int smoke_per_day = Integer.parseInt(str_smoke_per_day);
-                    int age = Integer.parseInt(str_age);
-                    intent.putExtra(v.getResources().getString(R.string.survey_smoke), smoke_per_day);
-                    intent.putExtra(v.getResources().getString(R.string.survery_age), age);
-                    intent.putExtra(v.getResources().getString(R.string.survey_gender), gender);
-
-                    // start next seal page
-                    startActivityForResult(intent, 1);
-                } else {
-                    SurveyErrorFragment newFragment = new SurveyErrorFragment();
-                    Bundle bundle = new Bundle();
-                    // pass validation result to dialog
-                    bundle.putBoolean("isAgeValid", isAgeValid);
-                    bundle.putBoolean("isSmokeValid", isSmokePerDayValid);
-                    bundle.putBoolean("isGenderValid", isGenderValid);
-                    newFragment.setArguments(bundle);
-                    newFragment.show(getSupportFragmentManager(), "missiles");
-                }
-            }
-        });
     }
 
     @Override
@@ -175,6 +190,23 @@ public class SurveyActivity extends AppCompatActivity implements IApprovePlanAsy
         isValid = isSmokePerDayValid && isGenderValid && isAgeValid;
 
         return isValid;
+    }
+
+    private boolean haveNetworkConnection() {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedWifi || haveConnectedMobile;
     }
 
     @Override
